ea0fb59c423682529bcf9ffa40bcf395a5bf83da
Anthony RIBON <aribon@e2r7p19.42.fr>
2015-01-10 16:45:58 +0100

    myncurses.cpp

---
+++ myncurses.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+#define Y_MAX 25
+#define X_MAX 80
+
+void		curses_init( void ) {
+
+	initscr(); /* Init the screen */
+	
+	noecho(); /* No echo in the screen */
+	curs_set(0); /* Hide the cursor */
+	resizeterm(Y_MAX, X_MAX); /* Resize the ncurse grid */
+
+	start_color(); 		/* Init color */
+    init_pair(1, 1, 0); /* RED */
+    init_pair(2, 2, 0); /* GREEN */
+    init_pair(3, 3, 0); /* YELLOW */
+    init_pair(4, 4, 0); /* BLUE */
+    init_pair(5, 5, 0); /* MAGENTA */
+    init_pair(6, 6, 0); /* CYAN */
+
+	return;
+}
+
+void 		curses_print( int y, int x, char c) {
+
+	mvprintw( y, x, "%c", c );
+	refresh();
+
+	return;
+}
+
+void 		curses_kill( void ) {
+
+	clear();
+	endwin();
+
+	return;
+}
